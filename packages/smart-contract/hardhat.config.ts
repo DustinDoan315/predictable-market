a9c2579fc7ecea440c67457d93228638b15ce31f
Dustin Doan <doankhanhdong3105@gmail.com>
2024-12-03 17:12:31 +0700

    add Polymarket contract

---
+++ packages/smart-contract/hardhat.config.ts
@@ -0,0 +1,43 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import { vars } from "hardhat/config";
+import "hardhat-tracer";
+
+// Environment variables
+const WALLET_SECRET_KEY = vars.get("WALLET_SECRET_KEY");
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const BSCSCAN_API_KEY = vars.get("BSCSCAN_API_KEY");
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.27",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    bsctest: {
+      url: `https://bsc-testnet.infura.io/v3/${INFURA_API_KEY}`,
+      chainId: 97,
+      accounts: WALLET_SECRET_KEY ? [WALLET_SECRET_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      bscTestnet: BSCSCAN_API_KEY,
+    },
+  },
+  sourcify: {
+    enabled: true,
+  },
+};
+
+process.on("warning", (warning) => {
+  if (warning.name === "ExperimentalWarning") {
+  }
+});
+
+export default config;
---
+++ packages/smart-contract/scripts/Polymarket.ts
@@ -0,0 +1,29 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  // const network = hardhatArguments.network ? hardhatArguments.network : 'dev';
+  const [deployer] = await ethers.getSigners();
+  const provider = ethers.provider;
+
+  const deployerBalance = await provider.getBalance(deployer.address);
+  console.log("deploy from address: ", deployer.address);
+
+  console.log("Deployer balance:", ethers.formatEther(deployerBalance));
+
+  const contractFactory = await ethers.getContractFactory("Polymarket");
+
+  const tokenAddress = "0x425eea9d65f20ce7FB56D810F8fD2697c717879a";
+
+  const contract = await contractFactory.deploy(tokenAddress, {
+    gasLimit: 1000000,
+  });
+
+  console.log("Contract deployed at: ", contract);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
